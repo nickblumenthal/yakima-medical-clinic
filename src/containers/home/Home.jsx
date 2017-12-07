ea686fcc86f58bf16e24d40e177a84127ac10562
Nick Blumenthal <nrblum01@gmail.com>
2017-12-06 19:32:03 -0800

    Home page polish and licenses

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -11,7 +11,7 @@ import TeamPhoto from 'team_large.jpg'
 import SynergyPhoto from 'synergy_logo.png'
 import MwlcyPhoto from 'mwlcy_logo.svg'
 import SuboxonePhoto from 'suboxone_logo.png'
-import { stethoscope, heart } from 'data_icons/stethoscope'
+import { stethoscope_2, heart_2 } from 'data_icons/stethoscope'
 
 class Home extends Component {
   render() {
@@ -30,7 +30,7 @@ class Home extends Component {
           <Col xs={12} lg={5}>
             <Media className="pb-4">
               <Media left className="pr-sm-2 d-none d-sm-block">
-                <Media object src={stethoscope} />
+                <Media object src={stethoscope_2} height="64" width="64"/>
               </Media>
               <Media body className="text-left">
                 <Media heading>
@@ -45,7 +45,7 @@ class Home extends Component {
           <Col xs={12} lg={5}>
             <Media>
               <Media left className="pr-sm-2 d-none d-sm-block">
-                <Media object src={heart} />
+                <Media object src={heart_2} height="64" width="64"/>
               </Media>
               <Media body className="text-left">
                 <Media heading>
@@ -56,39 +56,38 @@ class Home extends Component {
             </Media>
           </Col>
         </Row>,
+        <Row noGutters className="justify-content-center"><Col xs={10} className="border border-top-0"></Col></Row>,
         <Row noGutters className="justify-content-around card-row py-2 p-sm-4" key={3}>
-          <Col xs={12} md={10} lg={3}>
-            <Card>
-              <CardImg top width="100%" src={SynergyPhoto} alt="Synergy MedAesthetics" />
+          <Col xs={12} md={10} lg={4} className="px-4">
+
+              <a href="http://synergymedaesthetics.com/"><img width="100%" src={SynergyPhoto} alt="Synergy MedAesthetics" /></a>
               <CardBody>
                 <CardTitle>A New You</CardTitle>
                 <CardSubtitle>Now Serving Tri-Cities</CardSubtitle>
                 <CardText>Synergy has partnered with Yakima Medical Center to offer cutting edge cosmetic and body sculpting procedures</CardText>
                 <CardLink href="http://synergymedaesthetics.com/">Visit</CardLink>
               </CardBody>
-            </Card>
+              <hr  className="d-lg-none"/>
           </Col>
-          <Col xs={12} md={10} lg={3}>
-            <Card>
-              <CardImg top width="100%" src={MwlcyPhoto} alt="Medical Weight Loss Center Yakima" />
+          <Col xs={12} md={10} lg={4} className="bordered-lg px-4">
+
+              <a href="http://mwlcy.com"><img width="100%" src={MwlcyPhoto} alt="Medical Weight Loss Center Yakima" /></a>
               <CardBody>
                 <CardTitle>Sustainable Weight Loss</CardTitle>
-                <CardSubtitle>with Candace Degesnstein</CardSubtitle>
                 <CardText>Meet your health and wellness goals with our specialized nutrition and exercise plans tailored to your specific needs.</CardText>
                 <CardLink href="mailto:scheduling@yakimamedicalclinic.com">Book a consultation</CardLink>
               </CardBody>
-            </Card>
+              <hr  className="d-lg-none"/>
           </Col>
-          <Col xs={12} md={10} lg={3}>
-            <Card>
-              <CardImg top width="100%" src={SuboxonePhoto} alt="Suboxone Logo" />
+          <Col xs={12} md={10} lg={4} className="px-4">
+
+              <a href="/suboxone_info.pdf"><img width="100%" src={SuboxonePhoto} alt="Suboxone Logo" /></a>
               <CardBody>
                 <CardTitle>Suboxone</CardTitle>
                 <CardSubtitle>Dependency Treatment</CardSubtitle>
                 <CardText>Yakima Medical Clinic specializes in Suboxone to help heal chemical dependency.  Our trained staff has years of experience in providing the tools necessary to reach full recovery.</CardText>
                 <CardLink href="/suboxone_info.pdf">Find out more</CardLink>
               </CardBody>
-            </Card>
           </Col>
         </Row>
     ]
